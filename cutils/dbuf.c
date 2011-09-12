016fa3b465f4136de52c7bf16eb216a3753b3490
MoserMichael <moser.michael@gmail.com>
2011-09-12 07:03:52 -0700

    added dbuf class (dynamic buffer)

---
+++ cutils/dbuf.c
@@ -0,0 +1,30 @@
+#include "dbuf.h"
+#include <string.h>
+
+int DBUF_add(  DBUF* buf, void *data, size_t data_size )
+{
+  size_t bs;
+  uint8_t *nb;
+  
+  bs = buf->buf_size;
+  if ( (buf->buf_used + data_size) > bs) {
+    do {
+      bs *= 2;
+    } while( (buf->buf_used + data_size) > bs );
+
+    nb = (uint8_t *) realloc( buf->buf, bs );
+    if (!nb) {
+      return -1;
+    }
+    buf->buf = nb;
+    buf->buf_size = bs;
+  }
+  
+  memcpy( buf->buf + buf->buf_used, data, data_size );
+  buf->buf_size = bs;
+  buf->buf_used += data_size;
+
+  return 0; 
+}
+
+
---
+++ cutils/dbuf.h
@@ -0,0 +1,77 @@
+#ifndef _DBUF_X_Y_Z_
+#define _DBUF_X_Y_Z_
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+
+#include <cutils/base.h>
+
+/**
+ * @defgroup DBUF
+ * @brief buffer that is owner  of stuff that grows dynamically as you add stuff to it;  
+ * This object owns the memory that it is holding.
+ * it also has length/size fields, no assumption data includes zeros or not - any data (not just strings).
+ * @{
+ */
+typedef struct tagDBUF {
+
+  uint8_t *buf;
+  size_t buf_used;
+  size_t buf_size;
+
+} DBUF;
+
+/**
+ * @brief initialise a buffer
+ */
+M_INLINE int DBUF_init( DBUF *buf, size_t init_size )
+{
+  buf->buf = malloc( init_size );
+  if (!buf->buf) {
+    return -1;
+  }
+  buf->buf_used = 0;
+  buf->buf_size = init_size;
+  return 0;
+}
+
+/**
+ * @brief free memory owned by a buffer
+ */
+M_INLINE void DBUF_free(DBUF *buf) {
+  if (buf->buf) {
+    free(buf->buf); 
+    buf->buf = 0;
+    buf->buf_used = buf->buf_size = 0;
+  }
+}
+
+/**
+ * @brief add stuff to a buffer
+ */
+int DBUF_add(  DBUF* buf, void *data, size_t data_size );
+
+
+M_INLINE  void * DBUF_buffer( DBUF *buf )
+{
+  return buf->buf;
+}
+
+M_INLINE size_t DBUF_size( DBUF *buf )
+{
+  return buf->buf_used;
+}
+ 
+/**
+ * @}
+ */
+
+#ifdef  __cplusplus
+}
+#endif
+
+
+#endif
+
