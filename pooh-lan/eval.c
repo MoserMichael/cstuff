f100b25709a071e6de6be66fd91b758c86571d8c
Michael Moser <moser.michael@gmail.com>
2013-02-18 10:11:23 -0500

    wrong trace for unary negate. fixed.

--- pooh-lan/eval.c
+++ pooh-lan/eval.c
@@ -974,7 +974,7 @@ void EVAL_do_expr( EVAL_CTX *out, AST_EXPRESSION *expr, EVAL_REF_KIND get_value_
       if (tracer)  {
 	 const char *op_name = get_op_name(expr->val.unary.op);  
          DBUF_add(  &tracer->text, " ", 1);
-         DBUF_add(  &tracer->text, op_name, 1 );
+         DBUF_add(  &tracer->text, op_name, strlen( op_name ) );
          DBUF_add(  &tracer->text, " ", 1);
       }	
       switch( expr->val.unary.op )
@@ -998,9 +998,6 @@ void EVAL_do_expr( EVAL_CTX *out, AST_EXPRESSION *expr, EVAL_REF_KIND get_value_
         EVAL_do_expr( out, expr->val.unary.expr, PARAM_BY_VAL, 0 );
 	break;
       case TK_OP_LOGICAL_NEGATE:
-        if (tracer)  {
-          DBUF_add(  &tracer->text, " ! ", 3 );
-        }	
         EVAL_do_expr( out, expr->val.unary.expr, PARAM_BY_VAL, 0 );
         ret = EVAL_THREAD_get_stack_top( cthread );
         ret = BINDING_DATA_follow_ref( ret );
