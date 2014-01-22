9c80fce6c91320ad567a582e98c55dbcb1584719
Michael Moser <moser.michael@gmail.com>
2014-01-22 06:08:27 -0500

    fix of build all script for building dbgmem:

--- build/build-all.sh
+++ build/build-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-TARGETS="make-system-test vtest cutils butils nutils tutils stack corothread pooh-lan evcorothread hutils webby iptools dbgmem"
+TARGETS="make-system-test vtest cutils butils nutils tutils stack corothread pooh-lan evcorothread hutils webby iptools preloadut/dbgmem"
 
 DIR=`dirname $0`
 
