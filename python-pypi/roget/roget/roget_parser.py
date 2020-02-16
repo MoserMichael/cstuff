8282dfe1cf347960e26c838f3173d2d20898318b
Michael Moser <mmoser@redhat.com>
2020-02-16 22:04:57 +0200

    more pylint issues.

--- python-pypi/roget/roget/roget_parser.py
+++ python-pypi/roget/roget/roget_parser.py
@@ -4,7 +4,6 @@ import sys
 import re
 import os
 import time
-import cgi
 
 __all__ = [ 'RogetBuilder', 'RogetThesaurus', 'RogetNode', 'Sense', 'HeadWord', 'RogetThesaususFormatterText', 'RogetThesaurusFormatterXML', 'ROGET_NODE_CATEGORY', 'ROGET_NODE_HEADWORD', 'ROGET_NODE_SENSE_GROUP', 'ROGET_NODE_SENSE', 'WORD_TYPE_NONE', 'WORD_TYPE_VERB', 'WORD_TYPE_NOUN', 'WORD_TYPE_ADJ', 'WORD_TYPE_ADVERB', 'WORD_TYPE_PHRASE' ]
 
@@ -15,14 +14,14 @@ try:
 except Exception:
     print( 'failed to load cPicle' )
 
-''' types of nodes in tree '''
+""" types of nodes in tree """
 ROGET_NODE_CATEGORY = 1
 ROGET_NODE_HEADWORD = 2
 ROGET_NODE_SENSE_GROUP = 4
 ROGET_NODE_SENSE     = 8
 
 
-''' types of words '''
+""" types of words """
 WORD_TYPE_NONE = 0
 WORD_TYPE_VERB = 1
 WORD_TYPE_NOUN = 2
@@ -30,28 +29,28 @@ WORD_TYPE_ADJ  = 3
 WORD_TYPE_ADVERB  =  4
 WORD_TYPE_PHRASE = 5
 
-''' last counter of nodes '''
+""" last counter of nodes """
 _lastInternalId = 1
 
 
 
 class RogetBuilder:
-    '''
+    """
         The main entry point of this library; builds an instances of RogetThesaurus
-    '''
+    """
     _RECURSION_LIMIT = 4500
     _VERBOSE = 1
 
     #_wordGroupBoundaryRe = re.compile( '((\&amp;c\s+(\([^\)]+\))?\s*[^\s\,\;]+\.?|\[[^]]+\]|[^;])+)' )
-    _wordGroupBoundaryRe = re.compile( '((\&amp;c\s+(\([^\)]+\))?\s*[^\s^\,^\;^\.]+|\[[^]]+\]|\.(?!\n)|[^\;^\.])+)' )
-    _wordBoundaryRe = re.compile( '((\&amp;c\s+(\([^\)]+\))?\s*[^\s^\,^\;^\.]+\.?|\[[^]]+\]|[^,])+)' )
-    _startHeadWordRe = re.compile("^\s*([0-9]+[a-z]*)?.\s*(\[[^\]]*\])?([^\-]+)\-")
-    _linkRe = re.compile( '\&amp;c\s+(\([^\)]+\))?\s*([^\s^\,^\;^\.]+)\.?' )
-    _commentRe = re.compile('\[([^\]]*)\]')
-    _attributeRe = re.compile('(N\.|Adj\.|Adv\.|V\.|Phr\.)')
-    _cleanupRe = re.compile('[\^\n]')
-    _cleanupRe2 = re.compile('\s\s+')
-    _numRe = re.compile('(\d+)')
+    _wordGroupBoundaryRe = re.compile( r'((\&amp;c\s+(\([^\)]+\))?\s*[^\s^\,^\;^\.]+|\[[^]]+\]|\.(?!\n)|[^\;^\.])+)' )
+    _wordBoundaryRe = re.compile( r'((\&amp;c\s+(\([^\)]+\))?\s*[^\s^\,^\;^\.]+\.?|\[[^]]+\]|[^,])+)' )
+    _startHeadWordRe = re.compile(r"^\s*([0-9]+[a-z]*)?.\s*(\[[^\]]*\])?([^\-]+)\-")
+    _linkRe = re.compile( r'\&amp;c\s+(\([^\)]+\))?\s*([^\s^\,^\;^\.]+)\.?' )
+    _commentRe = re.compile(r'\[([^\]]*)\]')
+    _attributeRe = re.compile(r'(N\.|Adj\.|Adv\.|V\.|Phr\.)')
+    _cleanupRe = re.compile(r'[\^\n]')
+    _cleanupRe2 = re.compile(r'\s\s+')
+    _numRe = re.compile(r'(\d+)')
 
     _headWordIndex = {}
     _senseIndex = {}
@@ -205,26 +204,26 @@ class RogetBuilder:
                 raise
 
     def parse(self):
-        '''
+        """
             parse the roget thesaursus
             returns an instance of RogetThesaurus
 
             Note that that file 10681-body.txt  must be in the same directory as the script roget.py
-        '''
+        """
         fpath = os.path.split( __file__ )
         rpath = os.path.join( fpath[0], '10681-body.txt' )
         if os.access( rpath, os.F_OK | os.R_OK ) ==  0:
             raise Exception("Roget thesaursus text file rpath has not been found")
 
         if self._VERBOSE != 0:
-           print("parsing file: ", rpath)
-           tm = time.clock()
+            print("parsing file: ", rpath)
+            tm = time.clock()
 
         root = RogetNode(ROGET_NODE_CATEGORY, 'root')
-        classMatch = re.compile("^CLASS");
+        classMatch = re.compile("^CLASS")
         divisionMatch = re.compile("^DIVISION")
         sectionMatch = re.compile("^SECTION")
-        subsectionMatch = re.compile("^[0-9]+\.? [A-Z][A-Z,\s]+")
+        subsectionMatch = re.compile(r"^[0-9]+\.? [A-Z][A-Z,\s]+")
 
         currentNode = None
         currentClass = None
@@ -286,7 +285,7 @@ class RogetBuilder:
                                     if currentSubSection != None:
                                         currentNode = RogetNode( ROGET_NODE_CATEGORY, None, currentSubSection )
                                     else:
-                                       currentNode = RogetNode( ROGET_NODE_CATEGORY, None, currentSection )
+                                        currentNode = RogetNode( ROGET_NODE_CATEGORY, None, currentSection )
                                     currentNode._key = passage.strip()
                                 else:
                                     self._parseHeadWords( currentNode, passage )
@@ -302,7 +301,7 @@ class RogetBuilder:
         return RogetThesaurus(root,self._headWordIndex, self._senseIndex)
 
     def load(self, file ):
-        '''
+        """
         loads an instance of roget thesaurus (if possible from pickled/serialized form)
 
         if file does not exist
@@ -318,7 +317,7 @@ class RogetBuilder:
         Reason for this seems to be that pickled format is much larger then text file;
         pickle adds the type of the class as first element of sexpression -
         so there is a lot of redundancy and pickled file is much larger than text file.
-        '''
+        """
         res = None
         if os.access( file, os.F_OK | os.R_OK ):
             print('file exists: ', file)
@@ -340,55 +339,55 @@ class RogetBuilder:
 
     def _loadFromFile( self, file ):
         try:
-            r = RogetThesaurus()
+            #r = RogetThesaurus()
             if self._VERBOSE != 0:
-               print("Load from file: ", file)
-               tm = time.clock()
+                print("Load from file: ", file)
+                tm = time.clock()
             curRecursionLimit = sys.getrecursionlimit()
-            if curRecursionLimit < self.RECURSION_LIMIT:
-                sys.setrecursionlimit( self.RECURSION_LIMIT )
+            if curRecursionLimit < self._RECURSION_LIMIT:
+                sys.setrecursionlimit( self._RECURSION_LIMIT )
             with open( file ) as f:
                 ret = pickle.load( f )
-                if curRecursionLimit < self.RECURSION_LIMIT:
+                if curRecursionLimit < self._RECURSION_LIMIT:
                     sys.setrecursionlimit( curRecursionLimit )
                 if self._VERBOSE != 0:
                     tm = time.clock() - tm
                 print("time to load from file: ", tm)
                 return ret
-        except Exception:
+        except Exception as e:
             print('Error while loading thesaurus', e)
             return None
 
     def _storeToFile( self, file, r ):
         try:
             if self._VERBOSE != 0:
-               print("Storing to file: ", file)
-               tm = time.clock()
+                print("Storing to file: ", file)
+                tm = time.clock()
             curRecursionLimit = sys.getrecursionlimit()
-            if curRecursionLimit < self.RECURSION_LIMIT:
-                sys.setrecursionlimit( self.RECURSION_LIMIT )
+            if curRecursionLimit < self._RECURSION_LIMIT:
+                sys.setrecursionlimit( self._RECURSION_LIMIT )
             print('storing to ' +  file)
             with open( file, 'w') as f:
                 pickle.dump( r, f )
-            if curRecursionLimit < self.RECURSION_LIMIT:
+            if curRecursionLimit < self._RECURSION_LIMIT:
                 sys.setrecursionlimit( curRecursionLimit )
             if self._VERBOSE != 0:
                 tm = time.clock() - tm
                 print("time to store to file: ", tm)
-        except Exception:
+        except Exception as e:
             print('Error while storing thesaurus', e)
             os.remove( file )
             return None
 
 
 class RogetNode:
-    '''
+    """
         RogetNode - the base class of all nodes maintained by Roget thesaurus
-    '''
-    def __init__(self, type, description, parent = None):
+    """
+    def __init__(self, typ, description, parent = None):
         global _lastInternalId
 
-        self._type = type
+        self._type = typ
         if description != None:
             self._description = description.strip()
         else:
@@ -413,7 +412,7 @@ class RogetNode:
         return ret
 
     def typeToString(self):
-        ''' returns the type o this node as a string '''
+        """ returns the type o this node as a string """
         if self._type == ROGET_NODE_CATEGORY:
             return "Category"
         elif self._type == ROGET_NODE_HEADWORD:
@@ -425,45 +424,47 @@ class RogetNode:
 
     @property
     def type(self):
-        ''' returns the type of this node as a integer '''
+        """ returns the type of this node as a integer """
         return self._type
 
     @property
     def key(self):
-        ''' the meaning/key of this node '''
+        """ the meaning/key of this node """
         return self._key
 
     @property
     def description(self):
-        ''' returns an optional description (in the text this appears as [ .... ] ) '''
+        """ returns an optional description (in the text this appears as [ .... ] ) """
         return self._description
 
     @property
     def parent(self):
-        ''' returns the parent node (one up in the ontology) '''
+        """ returns the parent node (one up in the ontology) """
         return self._parent
 
     @property
     def child(self):
-        ''' returns the array of child nodes '''
+        """ returns the array of child nodes """
         return self._child
 
     @property
     def internalId(self):
-        ''' each node has its own internal id '''
+        """ each node has its own internal id """
         return self._internalId
 
 
 
 class Sense(RogetNode):
-    '''
+    """
         a single sense (the leaf node of the Roget Thesaurus
-    '''
-    def __init__(self, parent):
-        self.__init__(self, ROGET_NODE_SENSE, parent)
+    """
 
-    def __init__(self, type, parent):
-        RogetNode.__init__( self, type, None, parent)
+
+    #def __init__(self, parent):
+    #    self.__init__(self, ROGET_NODE_SENSE, parent)
+
+    def __init__(self, typ, parent):
+        RogetNode.__init__( self, typ, None, parent)
         self._comment = ''
         self._link = None
         self._linkComment = None
@@ -475,18 +476,18 @@ class Sense(RogetNode):
     def _wordToString(self):
         ret = self.typeToString() + " ("  + self.key + ")"
         if self._wordType != WORD_TYPE_NONE:
-             ret += ' /'
-             if self._wordType ==  WORD_TYPE_ADJ:
+            ret += ' /'
+            if self._wordType ==  WORD_TYPE_ADJ:
                 ret += 'Adj'
-             if self._wordType ==  WORD_TYPE_VERB:
+            if self._wordType ==  WORD_TYPE_VERB:
                 ret += 'V'
-             if self._wordType ==  WORD_TYPE_ADVERB:
+            if self._wordType ==  WORD_TYPE_ADVERB:
                 ret += 'Adv'
-             if self._wordType ==  WORD_TYPE_NOUN:
+            if self._wordType ==  WORD_TYPE_NOUN:
                 ret += 'N'
-             if self._wordType ==  WORD_TYPE_PHRASE:
+            if self._wordType ==  WORD_TYPE_PHRASE:
                 ret += 'Phr'
-             ret += '/ '
+            ret += '/ '
         if self.comment != '':
             ret += ' comment: ' + self.comment
         if self.link != None:
@@ -498,31 +499,31 @@ class Sense(RogetNode):
 
     @property
     def comment(self):
-        ''' an optional comment (in the text this is the text that appears in brackets ) '''
+        """ an optional comment (in the text this is the text that appears in brackets ) """
         return self._comment
 
     @property
     def link(self):
-        ''' optional link to a node of type HeadWord (in the text this appears as "&amp;c; 111" - link to headword with id 111 '''
+        """ optional link to a node of type HeadWord (in the text this appears as "&amp;c; 111" - link to headword with id 111 """
         return self._link
 
     @property
     def linkComment(self):
-        ''' optional comment on a link '''
+        """ optional comment on a link """
         return self._linkComment
 
     @property
     def wordType(self):
-        ''' optional word type annotation '''
+        """ optional word type annotation """
         return self._wordType
 
 
 
 
 class HeadWord(Sense):
-    '''
+    """
         A headword
-    '''
+    """
     def __init__(self, HeadIndex, parent):
         Sense.__init__( self, ROGET_NODE_HEADWORD, parent)
         self._index = HeadIndex.strip()
@@ -532,17 +533,17 @@ class HeadWord(Sense):
 
     @property
     def index(self):
-        ''' the string id that identifies the headword in the Roget thesaurus '''
+        """ the string id that identifies the headword in the Roget thesaurus """
         return self._index
 
-'''
-    The Roget Thesaurus class
-'''
 class RogetThesaurus:
-    def __init__(self):
-        self._rootNode = None
-        self._headWordIndex = None
-        self._senseIndex = None
+    """ class Roget
+        The Roget Thesaurus class
+    """
+    #def __init__(self):
+    #    self._rootNode = None
+    #    self._headWordIndex = None
+    #    self._senseIndex = None
 
     def __init__(self, rootNode = None, headWordIndex = None, senseIndex = None ):
         self._rootNode = rootNode
@@ -551,17 +552,17 @@ class RogetThesaurus:
 
     @property
     def rootNode(self):
-        ''' the root node of the ontology '''
+        """ the root node of the ontology """
         return self._rootNode
 
     @property
     def headWordIndex(self):
-        ''' the index of head words - maps a head word to its node in the ontology '''
+        """ the index of head words - maps a head word to its node in the ontology """
         return self._headWordIndex
 
     @property
     def senseIndex(self):
-        ''' the index of word senses - maps the word sense to a list of nodes in the ontology '''
+        """ the index of word senses - maps the word sense to a list of nodes in the ontology """
         return self._senseIndex
 
     # add all parent nodes to array (up to first category); adds the
@@ -591,7 +592,7 @@ class RogetThesaurus:
         return ret
 
     def semanticSimilarity( self, seq1, seq2 ):
-        ''' computes the semantic similarity between two terms,
+        """ computes the semantic similarity between two terms,
 
             returns the following tuple (similarity-score, common-node-in-roget-thesaurus)
 
@@ -604,7 +605,7 @@ class RogetThesaurus:
 
             common-node-in-roget-thesaurus: is None if the score is 0;
             otherwise it is the common node that the score is based on
-        '''
+        """
         arr1 = self._semHelpSortedSet( seq1 )
         arr2 = self._semHelpSortedSet( seq2 )
 
@@ -643,9 +644,9 @@ class RogetThesaurus:
         return (score, rnode)
 
 class RogetThesaususFormatterText:
-    '''
+    """
         class for formatting of Roget thesaurus as text report
-    '''
+    """
     def show(self, roget, file, mask = 0xF ):
         self._showRogetTextImp( roget.rootNode, 1, file, mask)
 
@@ -653,7 +654,7 @@ class RogetThesaususFormatterText:
 
         if node.type & mask != 0:
             line = ''
-            for i in range(1,depth):
+            for _ in range(1,depth):
                 line += '\t'
             line += node.toString() + '\n'
             file.write( line )
@@ -661,17 +662,17 @@ class RogetThesaususFormatterText:
         for n  in node.child:
             self._showRogetTextImp(n , depth + 1, file, mask )
 
-def escape( str ):
-    str = str.replace("<", "&lt;")
-    str = str.replace(">", "&gt;")
-    str = str.replace("\"", "&quot;")
-    return str
+def escapeStr( s ):
+    s = s.replace("<", "&lt;")
+    s = s.replace(">", "&gt;")
+    s = s.replace("\"", "&quot;")
+    return s
 
 
 class RogetThesaurusFormatterXML:
-    '''
+    """
         class for formatting of Roget thesaurus as xml
-    '''
+    """
     def show(self, roget, file):
 
         file.write( "<?xml version='1.0'?>\n<rogetThesaurus>\n<ontology>" )
@@ -684,32 +685,32 @@ class RogetThesaurusFormatterXML:
         txt = "<" + node.typeToString() + " "
 
         if node.type == ROGET_NODE_CATEGORY:
-            txt += 'name="' + escape( node.key ) + '"'
+            txt += 'name="' + escapeStr( node.key ) + '"'
         #elif node.type == ROGET_NODE_SENSE_GROUP:
             # nothing to do
         elif node.type == ROGET_NODE_SENSE or node.type == ROGET_NODE_HEADWORD:
             if node.type == ROGET_NODE_HEADWORD:
-                txt += 'id="' + escape( node.index ) + '" '
-            txt += 'sense="' + escape( node.key ) + '" '
+                txt += 'id="' + escapeStr( node.index ) + '" '
+            txt += 'sense="' + escapeStr( node.key ) + '" '
             if node.wordType != WORD_TYPE_NONE:
-                 txt += ' wordType="'
-                 if node.wordType ==  WORD_TYPE_ADJ:
+                txt += ' wordType="'
+                if node.wordType ==  WORD_TYPE_ADJ:
                     txt += 'Adj'
-                 if node.wordType ==  WORD_TYPE_VERB:
+                if node.wordType ==  WORD_TYPE_VERB:
                     txt += 'V'
-                 if node.wordType ==  WORD_TYPE_ADVERB:
+                if node.wordType ==  WORD_TYPE_ADVERB:
                     txt += 'Adv'
-                 if node.wordType ==  WORD_TYPE_NOUN:
+                if node.wordType ==  WORD_TYPE_NOUN:
                     txt += 'N'
-                 if node.wordType ==  WORD_TYPE_PHRASE:
+                if node.wordType ==  WORD_TYPE_PHRASE:
                     txt += 'Phr'
-                 txt += '" '
+                txt += '" '
             if node.comment != '':
-                txt += ' comment="' + escape( node.comment ) + '" '
+                txt += ' comment="' + escapeStr( node.comment ) + '" '
             if node.link != None:
-                txt += ' link="' + escape( node.link.index ) + '" '
+                txt += ' link="' + escapeStr( node.link.index ) + '" '
                 if node.link.key != None:
-                    txt  += ' linkComment="' + escape( node.link.key )+ '" '
+                    txt  += ' linkComment="' + escapeStr( node.link.key )+ '" '
         txt += ">"
         file.write( txt + '\n' )
 
